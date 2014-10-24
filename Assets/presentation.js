6c7d9767297c5f09b1c0613336891c30b0919074
bdegiglio-onco <ben.degiglio@oncoservices.com>
2014-10-24 09:50:23 -0400

    Added shared assets file

---
+++ Assets/presentation.js
@@ -0,0 +1,37 @@
+
+window.presentationData = {
+    title: 'Demonstration',
+    //'00:00:58'
+    slides: ['0',
+'00:00:09',
+'00:00:35',
+'00:00:48',
+'00:01:01',
+'00:01:54',
+'00:02:12',
+'00:02:33',
+'00:02:50',
+'00:03:06',
+'00:03:19',
+'00:03:46',
+'00:04:11',
+'00:04:25',
+'00:04:45',
+'00:05:02',
+'00:05:26'
+],
+    markers: [{
+        title: 'Introduction',
+        timeStart: '00:00:00',
+        timeEnd: '00:00:31'
+    }, {
+        title: 'Mesothelioma',
+        timeStart: '00:00:32',
+        timeEnd: '00:06:35'
+    }, {
+        title: 'Non-Small Cell Lung Cancer',
+        timeStart: '00:06:36',
+        timeEnd: '00:25:44'
+	}]
+
+};
---
+++ Assets/zencoder_json.js
@@ -0,0 +1,37 @@
+{
+ "utf8":"✓",
+ "authenticity_token":"mrLqOIBoo1oh8mNexGBp550Ww9Kvlk3O6FuWEexcsic=",
+ "input":"https://dl.dropboxusercontent.com/s/d0pm9fja7d8quqq/CSP%20CRC_V2.mp4",
+ "output":[
+   {
+     "base_url":"http://primepresentations.s3.amazonaws.com/2014/esmo/csp/crc/videos/",
+     "filename":"video.mp4",
+     "format":"mp4",
+     "video_codec":"h264",
+     "audio_codec":"aac",
+     "width": "470",
+     "height": "344",
+     "aspect_mode": "pad"
+   },
+   {
+     "base_url":"http://primepresentations.s3.amazonaws.com/2014/esmo/csp/crc/videos/",
+     "filename":"video.webm",
+     "format":"webm",
+     "video_codec":"vp8",
+     "audio_codec":"vorbis",
+     "width": "470",
+     "height": "344",
+     "aspect_mode": "pad"
+   },
+   {
+     "base_url":"http://primepresentations.s3.amazonaws.com/2014/esmo/csp/crc/videos/",
+     "filename":"video.ogv",
+     "format":"ogv",
+     "video_codec":"theora",
+     "audio_codec":"vorbis",
+     "width": "470",
+     "height": "344",
+     "aspect_mode": "pad"
+   }]
+}
+
