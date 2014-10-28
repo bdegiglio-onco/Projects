9820ba30e5f408d58715f1c10f844687dfe38614
timoz888 <tim.waindi@oncoservices.com>
2014-10-28 17:04:45 -0400

    TimW 10/28 End

---
+++ Active/PB4LSS079-web-ESMO-GEP-NETs-TW/Case2/presentations.js
@@ -0,0 +1,42 @@
+window.presentationData = {
+    title: 'Demonstration',
+    //'00:00:58'
+    slides: [
+
+'00:00:00',
+'00:00:23',
+'00:01:54',
+'00:04:06',
+'00:05:50',
+'00:06:19',
+'00:06:56',
+'00:07:27',
+'00:07:31',
+'00:08:32',
+'00:09:29',
+'00:10:11',
+'00:10:18',
+'00:10:38',
+'00:12:34',
+'00:12:55',
+'00:13:06',
+'00:14:00'
+
+
+
+],
+    markers: [{
+        title: 'Introduction',
+        timeStart: '00:00:00',
+        timeEnd: '00:00:31'
+    }, {
+        title: 'Mesothelioma',
+        timeStart: '00:00:32',
+        timeEnd: '00:06:35'
+    }, {
+        title: 'Non-Small Cell Lung Cancer',
+        timeStart: '00:06:36',
+        timeEnd: '00:25:44'
+	}]
+
+};
