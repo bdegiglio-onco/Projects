3967de65c5ac9fc8521075502f80549b0991674b
timoz888 <tim.waindi@oncoservices.com>
2014-11-05 09:35:25 -0500

    Tim W
    Merge branch 'master' of https://github.com/bdegiglio-onco/Projects
    
    Tim W
    
    added new projects to active folder

---
+++ Active/PB4LSS080-web-EANO-TW/server/perry/presentations.js
@@ -0,0 +1,40 @@
+window.presentationData = {
+    title: 'Demonstration',
+    //'00:00:58'
+    slides: [
+
+'00:00:00',
+'00:00:25',
+'00:01:20',
+'00:02:40',
+'00:04:07',
+'00:04:34',
+'00:05:19',
+'00:06:24',
+'00:06:57',
+'00:07:35',
+'00:08:52',
+'00:10:10',
+'00:10:46',
+'00:11:34',
+'00:11:57',
+'00:12:17',
+'00:12:34',
+'00:13:11'
+
+],
+    markers: [{
+        title: 'Introduction',
+        timeStart: '00:00:00',
+        timeEnd: '00:00:31'
+    }, {
+        title: 'Mesothelioma',
+        timeStart: '00:00:32',
+        timeEnd: '00:06:35'
+    }, {
+        title: 'Non-Small Cell Lung Cancer',
+        timeStart: '00:06:36',
+        timeEnd: '00:25:44'
+	}]
+
+};
---
+++ Active/PB4LSS080-web-EANO-TW/server/weller/presentations.js
@@ -0,0 +1,42 @@
+window.presentationData = {
+    title: 'Demonstration',
+    //'00:00:58'
+    slides: [
+
+'00:00:00',
+'00:00:16',
+'00:00:30',
+'00:01:00',
+'00:01:47',
+'00:02:45',
+'00:03:57',
+'00:04:24',
+'00:04:54',
+'00:05:10',
+'00:06:28',
+'00:06:45',
+'00:07:07',
+'00:07:19',
+'00:07:45',
+'00:08:05',
+'00:08:55',
+'00:10:48'
+
+
+
+],
+    markers: [{
+        title: 'Introduction',
+        timeStart: '00:00:00',
+        timeEnd: '00:00:31'
+    }, {
+        title: 'Mesothelioma',
+        timeStart: '00:00:32',
+        timeEnd: '00:06:35'
+    }, {
+        title: 'Non-Small Cell Lung Cancer',
+        timeStart: '00:06:36',
+        timeEnd: '00:25:44'
+	}]
+
+};
