6a8f2b698bdaef99d41bb11432f92802f81ea070
timoz888 <tim.waindi@oncoservices.com>
2014-11-07 14:46:21 -0500

    Friday changes

---
+++ Active/PI4VPO102-ESMO VPS-web-TW/ramalingham/presentation.js
@@ -0,0 +1,93 @@
+window.presentationData = {
+    title: 'Abstract 4970',
+    //'00:00:58'
+    slides: [
+	
+	
+'00:00:00',
+'00:00:34',
+'00:00:59',
+'00:02:09',
+'00:03:12',
+'00:04:04',
+'00:04:58',
+'00:05:18',
+'00:05:42',
+'00:06:17',
+'00:06:42',
+'00:08:31',
+'00:08:50',
+'00:09:10',
+'00:09:21',
+'00:09:34',
+'00:10:08'
+
+
+],
+    markers: [{
+        title: 'Introduction',
+        timeStart: '',
+        timeEnd: '00:00:30'
+    }, {
+        title: 'Rationale',
+        timeStart: '',
+        timeEnd: '00:02:11'
+    }, {
+        title: 'DREAM Design',
+        timeStart: '',
+        timeEnd: '00:03:04'
+	}, {
+        title: 'Main Eligibility Criteria',
+        timeStart: '',
+        timeEnd: '00:03:34'
+	}, {
+        title: 'Endpoints',
+        timeStart: '',
+        timeEnd: '00:04:38'
+	}, {
+        title: 'CONSORT Diagram',
+        timeStart: '',
+        timeEnd: '00:05:20'
+	}, {
+        title: 'Patient Characteristics',
+        timeStart: '',
+        timeEnd: '00:05:44'
+	}, {
+        title: 'Treatment Delivery',
+        timeStart: '',
+        timeEnd: '00:06:23'
+	}, {
+        title: 'Maintenance Progression-Free Survival',
+        timeStart: '',
+        timeEnd: '00:07:01'
+	}, {
+        title: 'Maintenance Overall Survival',
+        timeStart: '',
+        timeEnd: '00:07:30'
+	}, {
+        title: 'Subgroup Analysis',
+        timeStart: '',
+        timeEnd: '00:08:17'
+	}, {
+        title: 'Maintenance Response Rate',
+        timeStart: '',
+        timeEnd: '00:08:37'
+	}, {
+        title: 'Toxicity - Any Grade',
+        timeStart: '',
+        timeEnd: '00:09:09'
+	}, {
+        title: 'Post-Progression Therapy',
+        timeStart: '',
+        timeEnd: '00:09:31'
+	}, {
+        title: 'Conclusions (1)',
+        timeStart: '',
+        timeEnd: '00:10:29'
+	}, {
+        title: 'Conclusions (2)',
+        timeStart: '',
+        timeEnd: '00:11:34'
+	}]
+
+};
