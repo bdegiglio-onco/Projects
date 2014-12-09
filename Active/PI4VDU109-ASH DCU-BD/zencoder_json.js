bd30154e7e847a4d8c82ab99baa1903da853406a
timoz888 <tim.waindi@oncoservices.com>
2014-12-09 16:51:11 -0500

    Tim W

--- Active/PI4VDU109-ASH DCU-BD/zencoder_json.js
+++ Active/PI4VDU109-ASH DCU-BD/zencoder_json.js
@@ -1,10 +1,10 @@
 {
  "utf8":"✓",
  "authenticity_token":"mrLqOIBoo1oh8mNexGBp550Ww9Kvlk3O6FuWEexcsic=",
- "input":"https://dl.dropboxusercontent.com/s/vw064k27avj9pmy/1 ON 1 MELETIOS.mov",
+ "input":"https://dl.dropboxusercontent.com/s/4x1eep0gobue5i7/1 ON 1 KENNETH JESUS.mov?",
  "output":[
    {
-     "base_url":"http://primepresentations.s3.amazonaws.com/2014/ash-dcu/mmrr/videos/",
+     "base_url":"http://primepresentations.s3.amazonaws.com/2014/ash-dcu/mmfl/videos/",
      "filename":"video.mp4",
      "format":"mp4",
      "video_codec":"h264",
@@ -14,7 +14,7 @@
      "aspect_mode": "pad"
    },
    {
-     "base_url":"http://primepresentations.s3.amazonaws.com/2014/ash-dcu/mmrr/videos/",
+     "base_url":"http://primepresentations.s3.amazonaws.com/2014/ash-dcu/mmfl/videos/",
      "filename":"video.webm",
      "format":"webm",
      "video_codec":"vp8",
@@ -24,7 +24,7 @@
      "aspect_mode": "pad"
    },
    {
-     "base_url":"http://primepresentations.s3.amazonaws.com/2014/ash-dcu/mmrr/videos/",
+     "base_url":"http://primepresentations.s3.amazonaws.com/2014/ash-dcu/mmfl/videos/",
      "filename":"video.ogv",
      "format":"ogv",
      "video_codec":"theora",
