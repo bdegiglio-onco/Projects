6bcf74117cbb3dd0ca7464b1a77e173367d9d926
timoz888 <tim.waindi@oncoservices.com>
2014-12-09 08:17:16 -0500

    Tim W

---
+++ Active/PI4VDU109-ASH DCU-BD/zencoder_json.js
@@ -0,0 +1,37 @@
+{
+ "utf8":"✓",
+ "authenticity_token":"mrLqOIBoo1oh8mNexGBp550Ww9Kvlk3O6FuWEexcsic=",
+ "input":"https://dl.dropboxusercontent.com/s/vw064k27avj9pmy/1 ON 1 MELETIOS.mov",
+ "output":[
+   {
+     "base_url":"http://primepresentations.s3.amazonaws.com/2014/ash-dcu/mmrr/videos/",
+     "filename":"video.mp4",
+     "format":"mp4",
+     "video_codec":"h264",
+     "audio_codec":"aac",
+     "width": "940",
+     "height": "480",
+     "aspect_mode": "pad"
+   },
+   {
+     "base_url":"http://primepresentations.s3.amazonaws.com/2014/ash-dcu/mmrr/videos/",
+     "filename":"video.webm",
+     "format":"webm",
+     "video_codec":"vp8",
+     "audio_codec":"vorbis",
+     "width": "940",
+     "height": "480",
+     "aspect_mode": "pad"
+   },
+   {
+     "base_url":"http://primepresentations.s3.amazonaws.com/2014/ash-dcu/mmrr/videos/",
+     "filename":"video.ogv",
+     "format":"ogv",
+     "video_codec":"theora",
+     "audio_codec":"vorbis",
+     "width": "940",
+     "height": "480",
+     "aspect_mode": "pad"
+   }]
+}
+
