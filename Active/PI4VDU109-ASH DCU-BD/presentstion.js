b20a8ebfaa9971468e3a54b65ed0c8ce120a889e
Ben DeGiglio <ben.degiglio@oncoservices.com>
2014-12-10 15:16:03 -0500

    changes

---
+++ Active/PI4VDU109-ASH DCU-BD/presentstion.js
@@ -0,0 +1,22 @@
+
+window.presentationData = {
+    title: 'Demonstration',
+    //'00:00:58'
+    slides: ['0'
+
+],
+    markers: [{
+        title: 'Introduction',
+        timeStart: '00:00:00',
+        timeEnd: '00:00:31'
+    }, {
+        title: 'Mesothelioma',
+        timeStart: '00:00:32',
+        timeEnd: '00:06:35'
+    }, {
+        title: 'Non-Small Cell Lung Cancer',
+        timeStart: '00:06:36',
+        timeEnd: '00:25:44'
+	}]
+
+};
