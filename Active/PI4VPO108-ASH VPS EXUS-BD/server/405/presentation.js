5826e21e80358a535095c1ba4738b7d0f49bbd94
timoz888 <tim.waindi@oncoservices.com>
2014-12-15 09:12:09 -0500

    Tim W

---
+++ Active/PI4VPO108-ASH VPS EXUS-BD/server/405/presentation.js
@@ -0,0 +1,45 @@
+
+window.presentationData = {
+    title: 'Demonstration',
+    //'00:00:58'
+    slides: [
+	
+'00:00:00',
+'00:00:14',
+'00:01:17',
+'00:02:23',
+'00:03:21',
+'00:03:49',
+'00:04:30',
+'00:05:31',
+'00:05:55',
+'00:06:00',
+'00:06:36',
+'00:06:46',
+'00:07:15',
+'00:08:29',
+'00:10:17'
+
+
+
+
+
+
+
+
+],
+    markers: [{
+        title: 'Introduction',
+        timeStart: '00:00:00',
+        timeEnd: '00:00:31'
+    }, {
+        title: 'Mesothelioma',
+        timeStart: '00:00:32',
+        timeEnd: '00:06:35'
+    }, {
+        title: 'Non-Small Cell Lung Cancer',
+        timeStart: '00:06:36',
+        timeEnd: '00:25:44'
+	}]
+
+};
---
+++ Active/PI4VPO108-ASH VPS EXUS-BD/server/829/presentation.js
@@ -0,0 +1,43 @@
+
+window.presentationData = {
+    title: 'Demonstration',
+    //'00:00:58'
+    slides: [
+	
+'00:00:00',
+'00:00:10',
+'00:00:50',
+'00:01:29',
+'00:02:07',
+'00:02:42',
+'00:03:28',
+'00:04:02',
+'00:05:04',
+'00:05:34',
+'00:07:28',
+'00:08:12'
+
+
+
+
+
+
+
+
+
+],
+    markers: [{
+        title: 'Introduction',
+        timeStart: '00:00:00',
+        timeEnd: '00:00:31'
+    }, {
+        title: 'Mesothelioma',
+        timeStart: '00:00:32',
+        timeEnd: '00:06:35'
+    }, {
+        title: 'Non-Small Cell Lung Cancer',
+        timeStart: '00:06:36',
+        timeEnd: '00:25:44'
+	}]
+
+};
