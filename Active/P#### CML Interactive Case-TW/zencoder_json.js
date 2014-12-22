63f0c7f33b4709c7b1f10e54f60a3bdfab7cf585
timoz888 <tim.waindi@oncoservices.com>
2014-12-22 09:38:18 -0500

    Tim W

--- Active/P#### CML Interactive Case-TW/zencoder_json.js
+++ Active/P#### CML Interactive Case-TW/zencoder_json.js
@@ -1,36 +1,36 @@
 {
  "utf8":"✓",
  "authenticity_token":"mrLqOIBoo1oh8mNexGBp550Ww9Kvlk3O6FuWEexcsic=",
- "input":"http://primepresentations.s3.amazonaws.com/2014/cml-jabbour12/Case2_Part2.mp4",
+ "input":"http://primepresentations.s3.amazonaws.com/2014/cml-jabbour12/Case2_Part1.mp4",
  "output":[
    {
      "base_url":"http://primepresentations.s3.amazonaws.com/2014/cml-jabbour12/videos/",
-     "filename":"video.mp4",
+     "filename":"c1.mp4",
      "format":"mp4",
      "video_codec":"h264",
      "audio_codec":"aac",
-     "width": "470",
-     "height": "344",
+     "width": "940",
+     "height": "560",
      "aspect_mode": "pad"
    },
    {
      "base_url":"http://primepresentations.s3.amazonaws.com/2014/cml-jabbour12/videos/",
-     "filename":"video.webm",
+     "filename":"c1.webm",
      "format":"webm",
      "video_codec":"vp8",
      "audio_codec":"vorbis",
-     "width": "470",
-     "height": "344",
+     "width": "940",
+     "height": "560",
      "aspect_mode": "pad"
    },
    {
      "base_url":"http://primepresentations.s3.amazonaws.com/2014/cml-jabbour12/videos/",
-     "filename":"video.ogv",
+     "filename":"c1.ogv",
      "format":"ogv",
      "video_codec":"theora",
      "audio_codec":"vorbis",
-     "width": "470",
-     "height": "344",
+     "width": "940",
+     "height": "560",
      "aspect_mode": "pad"
    }]
 }
