068dcfb9fd143d4257afd2b46a32bbd5ccfccf4a
Ben DeGiglio <ben.degiglio@oncoservices.com>
2015-03-27 09:20:59 -0400

    changes

---
+++ Complete/SABCS exUS/cortes/presentation.js
@@ -0,0 +1,37 @@
+
+window.presentationData = {
+    title: 'Demonstration',
+    //'00:00:58'
+    slides: [
+'00:00:00',
+'00:07:52',
+'00:08:17',
+'00:08:30',
+'00:09:20',
+'00:10:22',
+'00:10:54',
+'00:11:05',
+'00:11:45',
+'00:12:15',
+'00:12:35',
+'00:12:42',
+'00:13:51',
+'00:14:01',
+'00:15:34'
+
+],
+    markers: [{
+        title: 'Introduction',
+        timeStart: '00:00:00',
+        timeEnd: '00:00:31'
+    }, {
+        title: 'Mesothelioma',
+        timeStart: '00:00:32',
+        timeEnd: '00:06:35'
+    }, {
+        title: 'Non-Small Cell Lung Cancer',
+        timeStart: '00:06:36',
+        timeEnd: '00:25:44'
+	}]
+
+};
