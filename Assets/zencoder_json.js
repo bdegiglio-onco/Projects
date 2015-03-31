70b8956e490df38c549f22ba62f02d8193aee730
Ben DeGiglio <ben.degiglio@oncoservices.com>
2015-03-31 09:19:09 -0400

    changes

--- Assets/zencoder_json.js
+++ Assets/zencoder_json.js
@@ -1,10 +1,10 @@
 {
  "utf8":"✓",
  "authenticity_token":"mrLqOIBoo1oh8mNexGBp550Ww9Kvlk3O6FuWEexcsic=",
- "input":"https://dl.dropboxusercontent.com/s/d0pm9fja7d8quqq/CSP%20CRC_V2.mp4",
+ "input":"https://dl.dropbox.com/s/3apx1m77xte0vfb/Toxic%20Skin%20Edit%207.mp4",
  "output":[
    {
-     "base_url":"http://primepresentations.s3.amazonaws.com/2014/esmo/csp/crc/videos/",
+     "base_url":"http://primepresentations.s3.amazonaws.com/2015/er-skin-toxicity/videos/",
      "filename":"video.mp4",
      "format":"mp4",
      "video_codec":"h264",
@@ -14,7 +14,7 @@
      "aspect_mode": "pad"
    },
    {
-     "base_url":"http://primepresentations.s3.amazonaws.com/2014/esmo/csp/crc/videos/",
+     "base_url":"http://primepresentations.s3.amazonaws.com/2015/er-skin-toxicity/videos/",
      "filename":"video.webm",
      "format":"webm",
      "video_codec":"vp8",
@@ -24,7 +24,7 @@
      "aspect_mode": "pad"
    },
    {
-     "base_url":"http://primepresentations.s3.amazonaws.com/2014/esmo/csp/crc/videos/",
+     "base_url":"http://primepresentations.s3.amazonaws.com/2015/er-skin-toxicity/videos/",
      "filename":"video.ogv",
      "format":"ogv",
      "video_codec":"theora",
